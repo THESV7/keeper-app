cea4af0573068bdb406bee337217078a5c3e8d77
THESV7 <vadhelsmit@gmail.com>
2023-07-21 21:04:09 +0530

    Keeper App Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Note from './components/Note';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
-        <Header />
-        <Note />
-        <Footer />       
+      <Header />
+      {notes.map((noteItem) => {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
